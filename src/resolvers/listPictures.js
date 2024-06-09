f056522bf0a56f3a34be322d6f79d9288739ce54
Richard Ward <Richard.Ward637@Outlook.com>
2024-06-09 19:44:26 +0100

    Fix list pictures

--- src/resolvers/listPictures.js
+++ src/resolvers/listPictures.js
@@ -5,7 +5,7 @@ const listPictures = async (_, __, { db, token }) => {
     return db('Media')
         .join('Mimetype', 'Media.mimetypeId', '=', 'Mimetype.id')
         .where('Mimetype.category', 'IMAGE')
-        .select('Media.*');
+        .select('Media.*', 'Mimetype.type as mimetype');
 };
 
 export default listPictures;

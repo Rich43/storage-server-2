197a5f515bfa44f77958331b1023d9ced020564f
Richard Ward <Richard.Ward637@Outlook.com>
2024-06-22 23:09:18 +0100

    Fix registerUser.js

--- src/resolvers/mutation/auth/registerUser.js
+++ src/resolvers/mutation/auth/registerUser.js
@@ -1,25 +1,32 @@
-import bcrypt from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
+import { hashPassword } from "../../utils.js";
 
 const registerUser = async (parent, { input }, { knex }) => {
     const { username, email, password } = input;
 
     // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = hashPassword(password);
+
+    // Check if this is the first user
+    const userCount = await knex('User').count('id as count');
+    const isFirstUser = userCount[0].count === 0;
 
     // Create the user in the database
     const newUser = {
-        id: uuidv4(),
         username,
-        email,
         password: hashedPassword,
-        created_at: new Date(),
-        updated_at: new Date()
+        admin: isFirstUser,  // first user is admin
+        created: knex.fn.now(),
+        avatar: null,
+        activated: isFirstUser,  // first user is activated
+        activation_key: uuidv4(),
+        banned: false,
+        updated: knex.fn.now()
     };
 
-    await knex('User').insert(newUser);
+    const [insertedUser] = await knex('User').insert(newUser).returning('*');
 
-    return newUser;
+    return insertedUser;
 };
 
 export default registerUser;

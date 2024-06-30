41997f66f4011cc875efdaeea591343411108536
Richard Ward <Richard.Ward637@Outlook.com>
2024-06-30 22:03:14 +0100

    Fix unit test

--- src/resolvers/mutation/deleteMedia.js
+++ src/resolvers/mutation/deleteMedia.js
@@ -1,26 +1,31 @@
-import { getUserFromToken, validateToken } from '../utils/utils.js';
+// noinspection ExceptionCaughtLocallyJS
 
-
+import { CustomError } from '../utils/CustomError';
 
 const deleteMedia = async (_, { id }, { db, model, utils, token }) => {
-    const session = await validateToken(db, token);
-    const user = await getUserFromToken(db, token);
+    try {
+        const session = await model.Session.validateToken(db, token);
+        const user = await model.User.getUserFromToken(db, token);
 
-    const media = model.Media.getMediaById(db, id);
-    if (!media) {
-        throw new Error('Media not found');
+        const media = await model.Media.getMediaById(db, id);
+        if (!media) {
+            throw new CustomError('Media not found');
+        }
+
+        if (media.userId !== session.userId) {
+            throw new CustomError('You can only delete your own media');
+        }
+
+        if (media.adminOnly && !user.admin) {
+            throw new CustomError('Only admins can delete adminOnly media');
+        }
+
+        await model.Media.deleteMediaById(db, id);
+        return true;
+    } catch (error) {
+        console.error(error);
+        throw new CustomError('Failed to delete media', error);
     }
-
-    if (media.userId !== session.userId) {
-        throw new Error('You can only delete your own media');
-    }
-
-    if (media.adminOnly && !user.admin) {
-        throw new Error('Only admins can delete adminOnly media');
-    }
-
-    await model.Media.deleteMediaById(db, id);
-    return true;
 };
 
 export default deleteMedia;

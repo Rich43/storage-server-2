ea8dac9f2da6e33f281ea8dc6ba07377b3054e5b
Richard Ward <Richard.Ward637@Outlook.com>
2024-06-30 21:44:06 +0100

    Fix unit test

--- src/resolvers/mutation/auth/setAvatar.js
+++ src/resolvers/mutation/auth/setAvatar.js
@@ -1,9 +1,12 @@
+// noinspection UnnecessaryLocalVariableJS,ExceptionCaughtLocallyJS
+
+import { CustomError } from "../../utils/CustomError.js";
+
 const setAvatar = async (parent, { mediaId }, { db, model, utils, token }) => {
     try {
         // Verify the user is authenticated
         const session = await model.Session.validateToken(db, token);
-        if (!token) {
-            // noinspection ExceptionCaughtLocallyJS
+        if (!session) {
             throw new Error('Not authenticated');
         }
 
@@ -11,20 +14,18 @@ const setAvatar = async (parent, { mediaId }, { db, model, utils, token }) => {
         const media = await model.Media.getFirstMediaItemWithImageMimetypeById(db, mediaId);
 
         if (!media) {
-            // noinspection ExceptionCaughtLocallyJS
             throw new Error('Media must have image mime type category');
         }
 
         // Update the user's avatar
         await model.User.updateUserAvatar(db, session.userId, mediaId);
 
-        // noinspection UnnecessaryLocalVariableJS
-        const updatedUser = model.User.getUserById(session.userId);
+        const updatedUser = await model.User.getUserById(db, session.userId);
 
         return updatedUser;
     } catch (error) {
         console.error(error);
-        throw new Error('Failed to set avatar');
+        throw new CustomError('Failed to set avatar', error);
     }
 };
 

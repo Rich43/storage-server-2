16a84794d59ede81fff82abeca3bc37d4d86e620
Richard Ward <Richard.Ward637@Outlook.com>
2024-07-01 17:52:11 +0100

    Remove util test

--- src/resolvers/utils/utils.js
+++ src/resolvers/utils/utils.js
@@ -1,17 +1,6 @@
-import moment from 'moment';
-import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
 import { getMediaKeywords, performFilter, performPagination, performSorting } from "./mediaUtils.js";
-
-export function getDates() {
-    const sessionExpireDateTime = moment().add(1, 'hour').utc().toISOString(); // 1 hour expiration in UTC
-    const sessionExpireDateTimeFormatted = moment(sessionExpireDateTime).utc().format('YYYY-MM-DD HH:mm:ss');
-    return { sessionExpireDateTime, sessionExpireDateTimeFormatted };
-}
-
-export function hashPassword(password) {
-    return crypto.createHash('sha3-512').update(password).digest('hex');
-}
+import { getDates, hashPassword } from "./misc.js";
 
 export const __ALL__ = {
     getDates, hashPassword, performFilter, performPagination,

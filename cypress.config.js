44613dcc3741b203df5c4e7e598e7201838c1626
Richie Ward <richies@gmail.com>
2024-11-22 19:18:22 +0000

    #9 - Initial bdd code

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,24 @@
 const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = preprocessor.addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
     e2e: {
-        specPattern: "cypress/e2e/**/*.feature", // Location of your .feature files
-        setupNodeEvents(on, config) {
-            preprocessor.addCucumberPreprocessorPlugin(on, config).then();
+        specPattern: "cypress/integration/**/*.feature", // Matches your feature file location
+        setupNodeEvents: async (on, config) => {
+            // Add the Cucumber preprocessor plugin
+            await addCucumberPreprocessorPlugin(on, config);
+
+            // Add the ESBuild preprocessor for parsing .feature files
+            on(
+                "file:preprocessor",
+                createBundler({
+                    plugins: [preprocessor.createEsbuildPlugin(config)],
+                })
+            );
+
+            // Return the updated config object
             return config;
         },
     },
